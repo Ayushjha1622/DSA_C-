b0164cb4b9e11481dc93405b15af1ad59fc5b571
Ayush jha <jhaa97204@gmail.com>
2025-06-24 22:33:59 +0530

    new file

---
+++ Linked_list/middle.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void findMiddle() {
+        if (head == NULL) {
+            cout << "List is empty\n";
+            return;
+        }
+
+        Node* slow = head;
+        Node* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        cout << "Middle element: " << slow->data << endl;
+    }
+
+    void printLL() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    List ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
+
+    ll.printLL();
+    ll.findMiddle();  // Output: Middle element: 3
+
+    return 0;
+}
